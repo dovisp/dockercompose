d7fcf81853ba8eef623bea96f4719bb352ec3af6
dovisp <64722645+dovisp@users.noreply.github.com>
2024-05-20 16:39:46 +0300

    Delete pihole.sh

--- pihole.sh
+++
@@ -1,3 +0,0 @@
-sudo sed -r -i.orig 's/#?DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
-sudo sh -c 'rm /etc/resolv.conf && ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf'
-systemctl restart systemd-resolved
